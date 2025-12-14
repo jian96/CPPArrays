a0b99e9cc7b74e580725668480390e7cbd42a1e6
Coal <y.liang1994@gmail.com>
2025-12-14 13:43:05 -0800

    learning templates

--- arrays.cpp
+++ arrays.cpp
@@ -1,34 +1,31 @@
-// from https://www.cs.uaf.edu/2009/spring/cs641/lecture/02_12_cache.html?utm_source=chatgpt.com
 #include <cstdio>
 #include <chrono>
-
+// this is not working properly, times are too same
 using namespace std::chrono;
 
-int main()
-{
-	time_point<system_clock> start = system_clock::now();
-	///printf("fuck you");
+template <typename F>
+void measure_time(const char* desc, F foo)
+{  //bool* showTimeScale) {
+    auto start = system_clock::now();
+    foo();
+    auto stop = system_clock::now();
+    auto elapsed_ns = duration_cast<nanoseconds>(stop - start).count();
+    auto elapsed_us = duration_cast<microseconds>(stop - start).count();
+    printf("%s: %lld microseconds, %lld nanoseconds\n", desc, elapsed_us, elapsed_ns);
+}
 
-	const int n = 1000;
-	int arr[n];
-	int i;
-	for (i = 0; i < n; i++)
-		arr[i] *= i;
-	time_point<system_clock> stop = system_clock::now();
-	time_t elapsed_ns = duration_cast<nanoseconds>(stop - start).count();
-	time_t elapsed_us = duration_cast<microseconds>(stop - start).count();
-	printf("time elapsed: %lld microseconds, %lld nanoseconds\n", elapsed_us, elapsed_ns);
+int main() {
+    const int n = 1000;
+    int arr[n];
+    measure_time("Multiply each element", [&]() {
+        for (int i = 0; i < n; i++)
+            arr[i] *= i;
+    });
 
-	const int m = 1000;
-	int arr_sec[m];
-	int j;
-	for (j = 0; j < m; j++)
-		arr_sec[0] *= j;
-	time_point<system_clock> start_2 = system_clock::now();
-	printf("--- accessing the same element %d times\n", m);
-
-	time_point<system_clock> stop_2 = system_clock::now();
-	time_t elapsed_ns_2 = duration_cast<nanoseconds>(stop_2 - start_2).count();
-	time_t elapsed_us_2 = duration_cast<microseconds>(stop_2 - start_2).count();
-	printf("time elapsed: %lld microseconds, %lld nanoseconds", elapsed_us_2, elapsed_ns_2);
+    const int m = 1000;
+    int arr_sec[m];
+    measure_time("Access same element", [&]() {
+        for (int j = 0; j < m; j++)
+            arr_sec[0] *= j;
+    });
 }
